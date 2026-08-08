f49fb92ca53e7ed704afa87aeeaf63b52ff6e4aa
Marko Pavlovic <markopavlo.69@gmail.com>
2026-08-08 12:20:11 +0200

    let ai finish the project

--- team.js
+++ team.js
@@ -2,28 +2,47 @@ const addBtn = document.querySelector("#addBtn");
 const memberInput = document.querySelector("#memberInput");
 const output = document.querySelector("#output");
 
+
 console.log(addBtn, memberInput);
 
 let members = [];
 
+
+
 addBtn.addEventListener("click", function(){
+    if (!memberInput.value.match(/^[a-zäöü]+$/i)) return;
     members.push(memberInput.value);
     render();
     
 })
 
 function render(){
+
+    if (members.length === 0){
+        output.textContent = "Build your Wuwa Team"
+    }
+    else{ 
     output.innerHTML = ""
-    //output.innerHTML += memberInput.value;
-    members.forEach((member) =>{
+    members.forEach((member, index) =>{
         
-        output.innerHTML += `
+        const teamBox = document.createElement("div");
+        teamBox.className = "teamBox";
+        output.append(teamBox);
+        const name = document.createElement("p");
+        name.textContent = member;
+        teamBox.append(name);
+        const delBtn = document.createElement("button");
+        delBtn.className = "delBtn";
+        delBtn.textContent = "remove"
+        teamBox.append(delBtn);
         
-        <p>${member}</p>
-        <button class=delBtn>Remove</button>
-        
-        `
-    })
+        delBtn.addEventListener("click", function(){
+            console.log(index);
+            members = members.filter((member, i) => i !== index);
+            render();
+           
+        })
+    })}
 
 }
 
